3ceae03f8f2886922a91f4e9d975a5d3e6218410
blueprintparadise <rhiray03@gmail.com>
2022-06-22 19:17:09 +0530

    Delete Runner.py

--- deepface/Runner.py
+++
@@ -1,5 +0,0 @@
-import os
-
-import DeepFace
-cwd = os.getcwd()
-DeepFace.stream(source=1,db_path = "images" ,model_name='Facenet',detector_backend = 'mtcnn',enable_face_analysis=False,time_threshold=1,frame_threshold=1)
